f030074523be6bcb4ff44979e12136e46e06d9f0
o34n22 <jimmyrevenga@googlemail.com>
2019-09-28 16:23:12 +0200

    tested

--- myapp/mathplay/nearest_number.py
+++ myapp/mathplay/nearest_number.py
@@ -9,6 +9,7 @@ number with two nearest permutations?
 
 import random as rd
 import numpy as np
+from math import ceil
 
 def permutations(n):
     """
@@ -77,66 +78,15 @@ def solve(n,target=1999):
         j+=1
     return P[j_min]
 
-def make_search_list(target=1999):
-    """
-    these lists should be written to file at some point
-    (only ~ 20 different ones exist)
-    """
-    L0 = list(range(10))
-    L = []
-    target_length = len(str(target))
-    target_rounded = round(target,-target_length+1)
-    d = int(str(target_rounded)[0])
-    if target_rounded - target > 0: 
-        d -= 1
-        while len(L) != 10:
-            L.append(L0.pop(d))
-            L.append(L0.pop(d))
-            if d != 0:
-                d -= 1
-        return L
-    else:
-        while len(L) != 10:
-            L.append(L0.pop(d))
-            if d != 0:
-                d -= 1
-            L.append(L0.pop(d))
-        return L
 
+def get_search_list(target=1999):
+    filename = 'search_lists.csv'
+    with open(filename) as f:
+        L = np.loadtxt(f,dtype=int,delimiter=' ')
+    power = len(str(target)) - 1
+    line_index = ceil(2*target*0.1**power)
+    return L[line_index]
     
-#def tail(n, R2, target=5472):
-#    n = str(n)
-#    for i in range(len(R2)):
-#        R2[i] = int(R2[i])        
-#    list.sort(R2)
-#    S = []
-#    # -------
-#    N = [int(d) for d in n]
-#    N.pop(N.index(R2[0]))
-#    N.sort(reverse=True)
-#    N = [R2[0]] + N
-#    s = ''
-#    for d in N:
-#        s += str(d)
-#    S.append(int(s))
-#    # -------
-#    N = [int(d) for d in n]
-#    N.pop(N.index(R2[1]))
-#    N.sort(reverse=False)
-#    N = [R2[1]] + N
-#    s = ''
-#    for d in N:
-#        s += str(d)
-#    S.append(int(s))
-#    # ------- distance to target
-#    S_ = S.copy()
-#    for i in range(len(S)):
-#        S_[i] -= target
-#        S_[i] = abs(S_[i])
-#    if S_[0] != S_[1]:
-#        min_index = min(range(len(S)),key=S_.__getitem__)
-#        return S[min_index]
-#    pass # catch case of two nearest permutations here
     
 def tail(n_tail,below=True):
     N = [int(d) for d in n_tail]
@@ -152,71 +102,64 @@ def solve_0(n, target=1999):
     aims to find the nearest number with the smallest number
     of comparisons needed
     """
-    n = str(n)
     if int(n) == target:
         return n
-    if len(n) == 2:
-        """only two digits left -> direct comparison"""
+    n = str(n)
+    if len(n) == 2: # only two digits left -> direct comparison
         distance_1 = abs(int(n) - target)
         distance_2 = abs(int(n[1]+n[0]) - target)
         if distance_1 < distance_2:
             return n
         else:
             return n[1] + n[0]
-    A = make_search_list(target=target)
-    print('A: ',A)
+    A = get_search_list(target=target)
     i = 0 # index in search list
-#    print("searching n = " + n)
     b = n.find(str(A[i]))
     if b != -1:
         inner0 = n[b]
-#        print(b)
-        print("found a " + inner0 + " at position " + str(b))
         i += 1
-        outer0 = None
-        while outer0 == None:
-            c = n.find(str(A[i]))
-            if c != -1:
-                outer0 = n[c]
-                print("found a " + outer0 + " at position " + str(c))
-            i += 1
-        if int(outer0) < A[0]:
-            outer = outer0 + tail(n[:c]+n[(c+1):],True)
+        c = n.find(str(A[i]))
+        outer0 = '-1'
+        if c != -1:
+            outer0 = n[c]
         else:
-            outer = outer0 + tail(n[:c]+n[(c+1):],False)
-#        print("outer: "+ str(outer))
+            new_n = n[:b]+n[(b+1):]
+            t = int(str(target)[1:])
+            inner = inner0 + solve_0(new_n,target=t)
+            return inner
+        if int(outer0) != -1 and int(outer0) < A[0]:
+            outer = outer0 + tail(n[:c]+n[(c+1):],below=True)
+        else:
+            outer = outer0 + tail(n[:c]+n[(c+1):],below=False)
         out_distance = abs(int(outer) - target)
         t = int(str(target)[1:])
-        print("target = " + str(target) + " ...new target = " + str(t))
         new_n = n[:b]+n[(b+1):]
-        print("     n = " + n + " ...new n =      " + str(new_n))
-        print("inner again")
         inner = inner0 + solve_0(new_n,target=t)
-#        print("inner: " + str(inner))
         in_distance = abs(int(inner) - target)
         if out_distance < in_distance:
             return outer
         else:
             return inner
     else:
-        print("hello from out here")
         i += 1
-        """if I didn't find i=0 search for two outers and compare directly"""
         while b == -1:
             b = n.find(str(A[i]))
             if b != -1:
                 outer0 = n[b]
                 if int(outer0) < A[0]:
-                    outer_1 = outer0 + tail(n[:b]+n[(b+1):],True)
+                    outer_1 = outer0 + tail(n[:b]+n[(b+1):],below=True)
                 else:
-                    outer_1 = outer0 + tail(n[:b]+n[(b+1):],False)
-                i += 1
+                    outer_1 = outer0 + tail(n[:b]+n[(b+1):],below=False)
+                if i < 9:
+                    i += 1
                 b = n.find(str(A[i]))
+                if b == -1:
+                    return outer_1
                 outer0 = n[b]
                 if int(outer0) < A[0]:
-                    outer_2 = outer0 + tail(n[:b]+n[(b+1):],True)
+                    outer_2 = outer0 + tail(n[:b]+n[(b+1):],below=True)
                 else:
-                    outer_2 = outer0 + tail(n[:b]+n[(b+1):],False)
+                    outer_2 = outer0 + tail(n[:b]+n[(b+1):],below=False)
                 break
             i += 1
         distance_1 = abs(int(outer_1) - target)
@@ -225,17 +168,7 @@ def solve_0(n, target=1999):
             return outer_1
         else:
             return outer_2
-#                    print('new_n: '+n[:c]+n[(c+1):])
-#                    ans += n[c]
-#                    return solve_0(n[:c]+n[(c+1):],int(t),ans=ans)
-#                else: 
-#                    return ans + tail1(n[:c]+n[(c+1):])
-#            print('just found something again!')
-#            print('namely a ' + n[b])
-#            print('at position '+ str(b))            
-            
-
-
+          
 
 def wrong(n,ans):
     if is_permutation(n,ans):
@@ -251,17 +184,22 @@ def wrong(n,ans):
 # --------------- testing ----------------
 
 
-#k = 0
-##L = [910,956,249,847,123]
-#while k < 10:
-#    number = rd.randint(1000,9999)
-##    number = L[k]
+k = 0
+#L = [910,956,249,847,123]
+while k < 10000:
+    number = rd.randint(1000,9999)
+#    number = L[k]
 #    print('number: '+str(number))
 #    print(int(solve(number)))
 #    print(solve_0(number))
-#    k += 1
-example_n = 1901
-print("number: " + str(example_n))
-print("correct solution:   " + solve(example_n))
-print("suggested solution: " + solve_0(example_n))
+    bum = int(solve(number)) - int(solve_0(number))
+    if bum != 0:
+        print('shit!')
+        print(number)
+    k += 1
+#example_n = 9999
+#print("number: " + str(example_n))
+#print("correct solution:   " + solve(example_n))
+#print("suggested solution: " + solve_0(example_n))
 
+#print(get_search_list(target=1999))
