eb715b6cf80cac84ba69e0c9450471ecd2a8450b
o34n22 <jimmyrevenga@googlemail.com>
2019-09-30 22:44:33 +0200

    update...

--- myapp/mathplay/nearest_number.py
+++ myapp/mathplay/nearest_number.py
@@ -2,94 +2,37 @@
 # -*- coding: utf-8 -*-
 
 
-"""
-fix:
-number with two nearest permutations?
-"""
-
+# todos: 
+# instructional text / message on what to do
+# control panel
+# 
+# 
+# 
+import os
 import random as rd
 import numpy as np
 from math import ceil
 
-def permutations(n):
-    """
-    takes some integer n as a str or int
-    produces list of all permutations
-    """
-    if type(n) == int: 
-        n = str(n)
-    digits = len(n)
-    if digits == 0:
-        return []
-    elif digits == 1:
-        return [n]
-    else:
-        L = [] 
-        for i in range(digits): # gotta start from tail!
-            for p in permutations(n[:i]+n[i+1:]):
-                L.append(n[i] + p)
-    return L
+base_dir = os.path.dirname(__file__)
 
-def is_permutation(n,m):
-    """
-    takes two integers n,m with same number of digits
-    and returns True if n is a permuation of m
-    """
-    n, m = str(n), str(m)
-    for i in n:
-        b = n.find(m[0])
-        if b == -1:
-            return False
-        m = m[1:]
-        n = n[:b] + n[(b+1):]
-    return True
-
-def pose(target=5000):
-    """
-    returns str
-    (a random integer with same number of digits as target number)
-    """
-    while True:
-        digits = len(str(target))
-        n = 0
-        for position in range(digits):
-            place_value = 10**position
-            n += place_value*rd.randint(0,9)
-        if len(str(n)) == digits: 
-            return str(n)
-
-# to-do: catch type errors
-
-def solve(n,target=5000):
-    """
-    solves by computing all permutations and the corresponding
-    distance to target
-    number of permutations ~ len(n)!
-    only for testing purposes
-    do not use in production!
-    """
-    P = permutations(n)
-    D = target*np.ones(len(P),dtype=int)
-    j = 0
-    j_min = 0
-    for p in P:
-        D[j] = abs(target - int(p))
-        if D[j] < D[j_min]: j_min = j
-        j+=1
-    return P[j_min]
+def pose(target):
+    number_of_digits = len(str(target))
+    low  = 10**(number_of_digits-1)
+    high = 10**number_of_digits - 1
+    return rd.randint(low,high)
 
 
-def get_search_list(target=5000):
+def get_search_list(target):
     number_of_digits = len(str(target))
     target = int(target)
-    filename = 'search_lists.csv'
+    filename = os.path.join(base_dir,'search_lists.csv')
     with open(filename) as f:
         L = np.loadtxt(f,dtype=int,delimiter=' ')
     power = number_of_digits - 1
     line_index = ceil(2*target*0.1**power)
     return L[line_index]
-    
-    
+
+
 def tail(n_tail,below=True):
     N = [int(d) for d in n_tail]
     N.sort(reverse=below)
@@ -97,16 +40,15 @@ def tail(n_tail,below=True):
     for d in N:
         s += str(d)
     return s
-    
 
-def solve_0(n, target=5000):
+
+def solve(n, target):
     """
     finds the permutation of n which is nearest to target
     """
     if int(n) == int(target):
         return n
     n = str(n)
-#    target = str(target)
     if len(n) == 2: # only two digits left -> direct comparison
         distance_1 = abs(int(n) - int(target))
         distance_2 = abs(int(n[1]+n[0]) - int(target))
@@ -115,12 +57,10 @@ def solve_0(n, target=5000):
         else:
             return n[1] + n[0]
     A = get_search_list(target=target)
-#    print(A)
     i = 0 # index in search list
     b = n.find(str(A[i]))
     if b != -1:
         inner0 = n[b]
-#        print('found ' + inner0 + " at position " + str(b))
         i += 1
         c = n.find(str(A[i]))
         outer0 = '-1'
@@ -129,7 +69,7 @@ def solve_0(n, target=5000):
         else:
             new_n = n[:b]+n[(b+1):]
             t = str(target)[1:]
-            inner = inner0 + solve_0(new_n,target=t)
+            inner = inner0 + solve(new_n,target=t)
             return inner
         if int(outer0) != -1 and int(outer0) < A[0]:
             outer = outer0 + tail(n[:c]+n[(c+1):],below=True)
@@ -138,7 +78,7 @@ def solve_0(n, target=5000):
         out_distance = abs(int(outer) - int(target))
         t = str(target)[1:]
         new_n = n[:b]+n[(b+1):]
-        inner = inner0 + solve_0(new_n,target=t)
+        inner = inner0 + solve(new_n,target=t)
         in_distance = abs(int(inner) - int(target))
         if out_distance < in_distance:
             return outer
@@ -176,39 +116,56 @@ def solve_0(n, target=5000):
             return outer_2
         else: # to be fixed...
             return outer_2
-          
 
-def wrong(n,ans):
-    if is_permutation(n,ans):
-        message = 'It seems there is a number yet nearer to 500.' # target!!!
-    else:
-        message = 'Digits aren\'t matching. You must use the digits from \
-                  the number given.'
-#    else:
-#        message = "I\'m speechless."
-    return message
+
+def is_permutation(n,m):
+    """
+    takes two integers n,m with same number of digits
+    and returns True if n is a permuation of m
+    """
+    n, m = str(n), str(m)
+    for i in n:
+        b = n.find(m[0])
+        if b == -1:
+            return False
+        m = m[1:]
+        n = n[:b] + n[(b+1):]
+    return True
+
+
+
+class Task:
+    def __init__(self,target):
+        self.target         = target        
+        self.number         = pose(target)
+        self.solution       = solve(self.number,self.target)
+        self.ans            = ''
+        self.description    = 'By moving around the digits of %s, find the number \
+            nearest to %s.' % (self.number,target)
+        self.blabla         = 'blabla'
+
+    def post_answer(self,some_answer):
+        self.ans      = some_answer
+        # todo: log posted answers
+        
+    def check_answer(self):
+        correct = self.solution == self.ans
+        return correct
+
+    def wrong(self):
+        """
+        yields a number of feedback messages if ans is False
+        """
+        if is_permutation(self.number,self.ans):
+            message = 'It seems there is a number yet nearer to %s.' % self.target
+        else:
+            message = 'Digits aren\'t matching. You must use the digits from \
+                      the number given.'
+    #    else:
+    #        message = "I\'m speechless."
+        return message
 
 
 # --------------- testing ----------------
+#a = Task(5001)
 
-
-k = 0
-#L = [910,956,249,847,123]
-while k < 1000:
-    number = rd.randint(1000,9999)
-#    number = L[k]
-#    print('number: '+str(number))
-#    print(int(solve(number)))
-#    print(solve_0(number))
-    bum = int(solve(number)) - int(solve_0(number))
-    if bum != 0:
-        print('shit!')
-        print('\n')
-#        print(number)
-    k += 1
-#example_n = 4532
-#print("number: " + str(example_n))
-#print("correct solution:   " + solve(example_n))
-#print("suggested solution: " + solve_0(example_n))
-
-#print(get_search_list(target=1999))
